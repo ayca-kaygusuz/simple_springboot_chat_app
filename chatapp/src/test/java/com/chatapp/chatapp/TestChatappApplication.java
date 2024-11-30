f8340031b271fcf1262acea46dc7b04460c55f6a
ayca-kaygusuz <aycakaygusuz@outlook.com>
2024-11-30 22:58:35 +0300

    Initialized simple models.

--- chatapp/src/test/java/com/chatapp/chatapp/TestChatappApplication.java
+++ chatapp/src/test/java/com/chatapp/chatapp/TestChatappApplication.java
@@ -2,6 +2,8 @@ package com.chatapp.chatapp;
 
 import org.springframework.boot.SpringApplication;
 
+import com.chatapp.ChatappApplication;
+
 public class TestChatappApplication {
 
 	public static void main(String[] args) {
